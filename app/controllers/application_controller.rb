0c64c5c6b174984f23decca806c3bf6a792fdd11
Maxim <polyakovmaxim05@gmail.com>
2022-12-23 14:41:38 -0500

    Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,26 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
-  #hello
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid
+  before_action :check_user
+
+  def invalid error
+    render json: { error: error.record.errors.full_messages}, status: :unprocessable_entity
+  end
+
+  def check_user 
+    unless current_user
+      render json: {message: "You're not logged in"}, status: 401
+    end
+  end
+
+  # def authorize 
+  #   !!current_user
+  # end
+
+   def current_user
+     User.find_by(id: session[:user_id])
+  end
+  
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,34 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first
+puts "sui"
+
+10.times{
+    User.create!(
+        username: Faker::App.name,
+        password: "123",
+        email: Faker::Internet.email,
+        wallet: 10
+    )
+}
+
+
+50.times{
+    Game.create!(
+        title: Faker::Game.title, 
+        genre: Faker::Game.genre,
+        price: rand(20..60).round(2),
+        platform: Faker::Game.platform
+    )
+}
+
+40.times{
+    Purchase.create!(
+        user_id: rand(1..10),
+        game_id: rand(1..50)
+    )
+}
+
+puts "suiiiiiii"
+    
